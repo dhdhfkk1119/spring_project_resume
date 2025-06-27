0fd71c621720ef8b65f256ca3714456065c83460
CliMinG0 <jhyeonjin365@gmail.com>
2025-06-27 16:00:51 +0900

    준비1

--- src/main/java/com/join/spring_resume/mypage/MypageController.java
+++ src/main/java/com/join/spring_resume/mypage/MypageController.java
@@ -1,4 +1,13 @@
 package com.join.spring_resume.mypage;
 
-public class mypage {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MypageController {
+
+    @GetMapping("/mypage")
+    public String mypage(){
+        return "mypage/mypage";
+    }
 }
