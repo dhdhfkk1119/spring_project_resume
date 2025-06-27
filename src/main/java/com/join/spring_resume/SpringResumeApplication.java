75a7a9bc91943aa4242546d1c3ff1194965dac91
whwwhwjddn123 <whwhwjddn123@gmail.com>
2025-06-27 11:12:01 +0900

    first

---
+++ src/main/java/com/join/spring_resume/SpringResumeApplication.java
@@ -0,0 +1,13 @@
+package com.join.spring_resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResumeApplication.class, args);
+	}
+
+}
