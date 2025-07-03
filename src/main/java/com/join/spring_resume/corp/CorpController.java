2d2b5dedf9147ed31b3779aae5b7f3160bd3a78d
whwwhwjddn123 <whwhwjddn123@gmail.com>
2025-07-03 14:03:31 +0900

    login , corp , member 유효성 검사

--- src/main/java/com/join/spring_resume/corp/CorpController.java
+++ src/main/java/com/join/spring_resume/corp/CorpController.java
@@ -1,12 +1,21 @@
 package com.join.spring_resume.corp;
 
+import com.join.spring_resume.member.Member;
 import com.join.spring_resume.session.SessionUser;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,23 +25,78 @@ public class CorpController {
     private final CorpService corpService;
     private final HttpSession session;
 
+    // 회원기입 화면
+    @GetMapping("/sign-form")
+    public String signForm(){
+        return "corp/sign-form";
+    }
 
+    @GetMapping("/login-form")
+    public String loginForm(){
+        return "corp/login-form";
+    }
+
+    // 로그인 화면
     @PostMapping("/login")
-    public String login(CorpRequest.LoginDTO loginDTO){
+    public String login(@Valid CorpRequest.LoginDTO loginDTO,
+                        BindingResult bindingResult,
+                        Model model){
 
-        Corp corp = corpService.login(loginDTO);
-        SessionUser sessionUser = SessionUser.fromCorp(corp);
-        session.setAttribute("session",sessionUser);
-
-        return "redirect:/";
+        if(bindingResult.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>(); // 해당 필드에 오류 메세지를 담아서 내보내기
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            model.addAttribute("errors", errorMap);
+            return "corp/login-form";
+        }
+        try{
+            Corp corp = corpService.login(loginDTO);
+            SessionUser sessionUser = SessionUser.fromCorp(corp);
+            session.setAttribute("session",sessionUser);
+            return "redirect:/";
+        } catch (Exception e) {
+            model.addAttribute("isErrors",e.getMessage());
+        }
+        return "corp/login-form";
     }
 
 
+
+
     @PostMapping("/corp-sign")
-    public String save(CorpRequest.saveDTO saveDTO){
-        System.out.println("기업 회원 가입 : " + saveDTO.toEntity());
-        Corp corp = corpService.save(saveDTO);
-        System.out.println("기업 회원 가입 : " + corp.getCorpId());
+    public String save(@Valid CorpRequest.saveDTO saveDTO,
+                       BindingResult bindingResult,
+                       Model model){
+
+        Map<String, String> errorMap = new HashMap<>(); // 에러 반딩 해주기 위해서 넘기기
+
+        if(bindingResult.hasErrors()){
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            model.addAttribute("errors", errorMap);
+            model.addAttribute("dto", saveDTO);
+            return "corp/sign-form";
+        }
+
+        // 비밀번호 체크
+        if (!saveDTO.getPassword().equals(saveDTO.getRePassword())) {
+            errorMap.put("rePassword", "비밀번호가 일치하지 않습니다.");
+            model.addAttribute("errors", errorMap);
+            model.addAttribute("dto", saveDTO);
+            return "corp/sign-form";
+        }
+
+        // 아이디 중복 체크
+        if (corpService.existsByCorpId(saveDTO.getCorpId())) {
+            errorMap.put("corpId", "이미 사용 중인 아이디입니다.");
+            model.addAttribute("errors", errorMap);
+            model.addAttribute("dto", saveDTO);
+            return "corp/sign-form";
+        }
+
+        corpService.save(saveDTO);
 
         return "redirect:/";
     }
--- src/main/java/com/join/spring_resume/member/MemberService.java
+++ src/main/java/com/join/spring_resume/member/MemberService.java
@@ -1,8 +1,11 @@
 package com.join.spring_resume.member;
 
+import com.join.spring_resume._core.errors.exception.Exception400;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.sound.midi.MetaMessage;
 
 @Service
 @RequiredArgsConstructor
@@ -11,19 +14,27 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member login(MemberRequest.LoginDTO loginDTO){
-        return memberRepository.findByMemberIdAndPassword(loginDTO.getMemberId(),loginDTO.getPassword())
-                .orElseThrow(() -> new RuntimeException("해당 아이디 및 유저를 찾을수 업습니다"));
+
+        Member member = memberRepository.findByMemberId(loginDTO.getMemberId())
+                .orElseThrow(() -> new Exception400("아이디 또는 비밀번호가 틀렸습니다"));
+
+        if (!member.getPassword().equals(loginDTO.getPassword())) {
+            throw new Exception400("아이디 또는 비밀번호가 틀립니다");
+        }
+
+        return member;
 
     }
 
 
     @Transactional
     public Member saveMember(MemberRequest.SaveDTO saveDTO){
-        System.out.println("Service 데이터 확인" + saveDTO.toEntity()); // 엔티티 확인
-        Member member = memberRepository.save(saveDTO.toEntity());
-        System.out.println("Service 데이터 저장: " + member.getMemberIdx());
-        System.out.println("Service 데이터 저장: " + member.getCreatedAt());
-        return member;
+
+        return memberRepository.save(saveDTO.toEntity());
+    }
+
+    public boolean existsByMemberId(String Id) {
+        return memberRepository.existsByMemberId(Id);
     }
 
 }
